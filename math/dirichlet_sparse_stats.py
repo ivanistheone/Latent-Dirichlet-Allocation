c45d38c22184f8b6b93be887d7956bd788fdb257
Ivan Savov <ivan.savov@gmail.com>
2010-12-18 00:05:36 -0500

    Exploring the sparse strucutre of Dirichlet distribution and debugging lots of stuff for the readers. MAC binary plists took my day away. I want it back Steve... why do you use binary formats? Oh why...

--- math/dirichlet_sparse_stats.py
+++ math/dirichlet_sparse_stats.py
@@ -5,30 +5,8 @@ import numpy
 import itertools
 
 
-def is_k_sparse(vec, k):
-    """
-    checks if 95% of the weight of a normalized vector vec
-    is in less than k of its entries.
-    """
-    WEIGHT=0.95     # 95%
-
-    n=len(vec)
-
-    if k<0 or k>n:
-        raise ValueError(" k has to be in 1 .. len(vec) ")
-
-    # a filter with k entries set to 1
-    exfilt = numpy.concatenate( (numpy.ones(k), numpy.zeros(n-k) ) )
-
-    out =  False
-
-    # go though all possible permutations of filter
-    for filt in itertools.permutations(exfilt):
-        if numpy.dot(vec,filt) >= WEIGHT:
-            out=True
-            break
-
-    return out
+DIR_DIM_START  = 1     # interested in many topics
+DIR_DIM_STOP   = 202    # but not too many ;)
 
 
 def get_sparse_stats(dsamples):
@@ -37,28 +15,36 @@ def get_sparse_stats(dsamples):
     and spits out the proportions of the samples
     that are k-sparse for k=1 ... n
 
+    define: k-sparse
+    checks if 95% of the weight of a normalized vector vec
+    is in less than k of its entries.
+
     returns an array of counts of sparseness
      [ # 1-sparse, # 2-sparse, # 3-sprase, ...,  # n-sparse ]
 
     note that we subtract from k-sparse all the k-1 sparse
     since we don't want to double count.
     """
+
+    WEIGHT=0.95     # 95%
+
     N,n = dsamples.shape        # N samples from, n-dim Dirichlet
 
-    out = numpy.zeros(n)
-    for k in range(1,n+1):
+    largelast = numpy.sort(dsamples,1)        #
 
-        count=0
-        for sample in dsamples:
-            if is_k_sparse(sample, k):
-                count +=1
-        if k==1:
-            out[0]=float(count)
-        else:
-            out[k-1]=float(count)-numpy.sum( out[0:k-1] )
+    counts = numpy.zeros(n)                # number of i-sparse entties
+    for sample in largelast:
 
+        k=1                 #
+        val=sample[n-k]     # start at last entry
+        # if it is already larger than 0.95 wont go into loop
+        while val < WEIGHT:     # loop until get enough bins to break out
+            k+=1
+            val = val + sample[n-k]
+        #
+        counts[k-1] +=1     # 0-based index
 
-    outf = numpy.divide(out,N)
+    outf = numpy.divide(counts,N)
     return outf
 
 
@@ -100,6 +86,102 @@ def get_sparse_stats(dsamples):
 # for \alpha = 50/n  explore what happens for different number of n
 # not currently possible with the itertools.permutations n=7 takes long already !
 # assuming T > 50 should be in the sparse regime ... but how sparse?
+
+
+
+
+def get_sparse_for_alpha(alpha, nrange=None, recomendalpha=False):
+    """
+     let's see a plot of sparseness
+     for different n
+     as a funciton of \alpha
+    """
+
+    N = 5000
+
+    #alpha = 0.01
+
+    if not nrange:
+        nrange = numpy.arange(DIR_DIM_START, DIR_DIM_STOP,1, dtype=float)
+
+    totaln = len(nrange)
+
+    interesting = 70    # show only sparseness up to k=30
+
+    results  =  []
+
+    #numpy.zeros( (totaln, interesting), dtype=float)         # each row represents the sparse
+                                                    # vector for a different n
+
+
+    for n in nrange:
+
+        # examine the 50/T suggestion
+        if recomendalpha:
+            alpha=50.0/n;
+        dirsamples = numpy.random.dirichlet( alpha * numpy.ones(n), N)
+        spvec = get_sparse_stats(dirsamples)
+
+        # in order to plot all n's together will pad with zeros
+        spvec_head = numpy.zeros(interesting)
+        for i in range(interesting):
+            if i <n:
+                spvec_head[i] = spvec[i]
+
+
+        results.append(spvec_head)  #[counter,:]= spvec_head
+
+    return numpy.array(results)
+
+
+
+
+
+def plot_reults( res ):
+    """
+    given a matrix of sparseness features
+    one in each row, plots a 3D figure with it
+
+    """
+
+    import pylab as p
+    import mpl_toolkits.mplot3d.axes3d as p3
+
+    #fig=p.figure()
+    #ax = p3.Axes3D(fig)
+
+
+    # setup the domain
+    x = numpy.arange(0, res.shape[0], 1)
+    y = numpy.arange(0, res.shape[1],1)
+    #DIR_DIM_START, DIR_DIM_STOP, 1)
+    X, Y = p.meshgrid(x, y)
+
+    p.pcolor(X,Y, res.T)
+    #ax.plot_wireframe(X,Y, res)
+    #ax.scatter3D(numpy.ravel(X), numpy.ravel(Y), numpy.ravel(res) )
+
+    #p.set_xlabel('Dirichlet dimension')
+    #x.set_ylabel('sparseness')
+    #ax.set_zlabel('prop of vectors of dim n with this sparsity')
+    #ax.
+
+    #fig.add_axes(ax)
+
+    p.show()
+
+
+
+#
+# mrecommend = get_sparse_for_alpha(0.1, recomendalpha=True)
+# plot_reults(mrecommend)
+# prior around small window... is this desired?
+
+
+
+
+
+
 
 
 
